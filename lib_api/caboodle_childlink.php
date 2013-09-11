9a7e77b88b4894b338488752529e3eec1650c2a9
Grzegorz Adamowicz (gstlt) <gadamowicz@gstlt.info>
2013-09-11 15:41:12 +0200

    Add basic api file

---
+++ lib_api/caboodle_childlink.php
@@ -0,0 +1,54 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Component 'caboodle', language 'en', branch 'MOODLE_24_STABLE'
+ *
+ * @package   caboodle
+ * @author    Grzegorz Adamowicz (greg.adamowicz@enovation.ie)
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once(dirname(__FILE__) . '/locallib.php');
+
+class caboodle_childlink extends caboodle_api {
+
+    public function __construct($resourceid, $instanceid, $numresults = 20) {
+        parent::__construct($resourceid, $instanceid, $numresults);
+    }
+
+    protected function search_api($query) {
+        $query = $this->clean_query_string($query);
+
+        $url = $this->url . '/' . $query;
+
+        if ($xmldata = $this->exec_curl($url)) {
+
+            $xmldata = $this->parse_data($xmldata);
+
+        } else {
+            return '';
+        }
+
+        return $xmldata;
+    }
+
+
+
+} // caboodle_childlink
